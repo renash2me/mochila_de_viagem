7bf3722efe0491df46b64e8b9be3c33264da02db
rezambelli <122014704+renash2me@users.noreply.github.com>
2023-08-04 08:47:26 -0300

    Corrigido
    Ao chamar a função "itemArmazenado", o atributo buscava 2 valores... então o resultado retornava erro.
    Alterei para o atributo para somente 1 valor "item" e o problema foi resolvido

--- js/main.js
+++ js/main.js
@@ -4,26 +4,22 @@ const lista = document.getElementById("lista")
 const itemArmazenado = JSON.parse(localStorage.getItem("armazenamentoDeItens")) || []
 
 itemArmazenado.forEach((item) => {
-    criaItem(item.nome, item.quantidade)
+    criaItem(item)
 })
 
 // Funções
 function criaItem (itemAdicionado) {
 
-    // Cria 'li' e atribui a classe ".item" a ela.
+       // Cria 'li' e atribui a classe ".item" a ela.
     const novoItemCadastrado = document.createElement('li')
     novoItemCadastrado.classList.add("item")
-
-    // Cria um 'strong' com a quantiade de objetos adicionados
+        // Cria um 'strong' com a quantiade de objetos adicionados
     const quantidadeItens = document.createElement('strong')
     quantidadeItens.innerHTML = itemAdicionado.quantidade
-
-
-    // Junta a criação da 'li', com a class ".item" e com o strong, montando a linha no formato correto
+        // Junta a criação da 'li', com a class ".item" e com o strong, montando a linha no formato correto
     novoItemCadastrado.appendChild(quantidadeItens)
     novoItemCadastrado.innerHTML += itemAdicionado.nome
-
-    // Insere a linha criada na lista pré-existente
+        // Insere a linha criada na lista pré-existente
     lista.appendChild(novoItemCadastrado)
 
 }
@@ -31,8 +27,7 @@ function criaItem (itemAdicionado) {
 // Scripts
 formulario.addEventListener("submit", (capturou) => {
     capturou.preventDefault()
-
-    // Cria constante para melhorar o código
+        // Cria constante para melhorar o código
     const nome = capturou.target.elements['nome']
     const quantidade = capturou.target.elements['quantidade']
 
